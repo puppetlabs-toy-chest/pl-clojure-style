dbfe1e731c142090a13a59a255272e9408585c0b
Patrick Carlisle <patrick@puppetlabs.com>
2016-02-05 14:31:04 -0800

    Don't remove consecutive blank lines.
    This is buggy and seems like something we should allow anyway.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pl-cljfmt "0.1.0-SNAPSHOT"
   :description "Puppetlabs cljfmt rules"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :cljfmt {:indents {defservice [[:inner 0] [:inner 1]]
+  :cljfmt {:remove-consecutive-blank-lines? false
+           :indents {defservice [[:inner 0] [:inner 1]]
                      try+ [[:block 0]]}}
   )
