a8a5e4c1e34dbc36c00a5d67eb8f97511658450d
Patrick Carlisle <patrick.carlisle@puppet.com>
2016-05-13 14:40:31 -0700

    Update emacs indentation
    The format for specifying indentation in cider has changed. This updates the
    existing emacs indentations to follow it.

--- indents.el
+++ indents.el
@@ -1,10 +1,20 @@
 ;; For use with clojure-mode in emacs
-(put 'GET 'clojure-backtracking-indent '(2))
-(put 'POST 'clojure-backtracking-indent '(2))
-(put 'PUT 'clojure-backtracking-indent '(2))
-(put 'DELETE 'clojure-backtracking-indent '(2))
-(put 'HEAD 'clojure-backtracking-indent '(2))
-(put 'ANY 'clojure-backtracking-indent '(2))
-(put 'context 'clojure-backtracking-indent '(2))
-(put 'defservice 'clojure-backtracking-indent '((2)))
+;; https://github.com/clojure-emacs/clojure-mode#indentation-of-macro-forms
+
+;; compojure
+(put 'defroutes 'clojure-indent-function :defn)
+(put 'GET 'clojure-indent-function 2)
+(put 'POST 'clojure-indent-function 2)
+(put 'PUT 'clojure-indent-function 2)
+(put 'DELETE 'clojure-indent-function 2)
+(put 'HEAD 'clojure-indent-function 2)
+(put 'ANY 'clojure-indent-function 2)
+(put 'context 'clojure-indent-function 2)
+
+;; trapperkeeper
+(put 'defservice 'clojure-indent-function '(1 nil (1)))
+(put 'service 'clojure-indent-function '(1 nil (1)))
+(put 'fnk 'clojure-indent-function :defn)
+
+;; slingshot (same as default try indentation in cider)
 (put 'try+ 'clojure-indent-function 0)
